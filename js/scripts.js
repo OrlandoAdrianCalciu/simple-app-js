ce195d946f86eeb8d9c09754c8e920690e32c9d9
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-05-18 11:19:22 +0100

    added an alert() to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,3 @@
-let pokemonList = [{name: 'Bulbasaur', height: 7, type: ['grass', 'poison']},{name: 'Charmander', height: 6, type: 'fire'},{name: 'Pidgey', height: 3, type:['flying', 'normal']}]
+alert('Hello world!')
+
+let pokemonList = [{ name: 'Bulbasaur', height: 7, type: ['grass', 'poison'] }, { name: 'Charmander', height: 6, type: 'fire' }, { name: 'Pidgey', height: 3, type: ['flying', 'normal'] }]
