d7f1207d822693c15d0786652d274e19be5d947d
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-05-18 14:35:55 +0100

    added for loops for the pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,41 @@
-alert('Hello world!')
+// alert('Hello world!')
 
-let favoriteFood = 'Pizza'
-document.write(favoriteFood)
 
-let pokemonList = [{ name: 'Bulbasaur', height: 7, type: ['grass', 'poison'] }, { name: 'Charmander', height: 6, type: 'fire' }, { name: 'Pidgey', height: 3, type: ['flying', 'normal'] }]
+// for (let i = 1; i <= 100; i++) {
+//     console.log(i);
+// }
+
+// let day = "Friday"
+
+// if(day === "Sunday"){
+//     console.log("YEEY! It's Sunday")
+// } else if(day === "Saturday"){
+//     console.log("Still weekend!!")
+// } else{
+//     console.log("Oh no!")
+// }
+
+
+// let favoriteFood = 'Pizza'
+// document.write(favoriteFood)
+
+let pokemonList = [
+    { name: 'Bulbasaur', height: 70, type: ['grass', 'poison'] },
+    { name: 'Charmander', height: 60, type: 'fire' },
+    { name: 'Pidgey', height: 30, type: ['flying', 'normal'] },
+    { name: 'Onix', height: 880, type: ['rock', 'ground'] },
+    { name: 'Ekans', height: 200, type: ['poison'] },
+]
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write("<br>" + pokemonList[i].name + " " + "(height: " + pokemonList[i].height + ")")
+
+    if (pokemonList[i].height >= 100) {
+        document.write(" - Wow, that's big!")
+    }
+    else if (pokemonList[i].height >= 50) {
+        document.write(" - That is avarage!")
+    } else {
+        document.write(" - That's so tiny!")
+    }
+}
