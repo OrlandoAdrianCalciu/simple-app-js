8d9105d54651df01bc9e88023326654e6c98f686
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-05-24 16:11:36 +0100

    updated scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,67 @@
 
 //FOREACH LOOPS
-let pokemonRepository = (function(){
+let pokemonRepository = (function () {
 
-let pokemonList = [
-    { name: 'Bulbasaur', height: 70, type: ['grass', 'poison'] },
-    { name: 'Charmander', height: 60, type: 'fire' },
-    { name: 'Pidgey', height: 30, type: ['flying', 'normal'] },
-    { name: 'Onix', height: 880, type: ['rock', 'ground'] },
-    { name: 'Ekans', height: 200, type: ['poison'] },
-]
+    let pokemonList = [
+        { name: 'Bulbasaur', height: 70, type: ['grass', 'poison'] },
+        { name: 'Charmander', height: 60, type: 'fire' },
+        { name: 'Pidgey', height: 30, type: ['flying', 'normal'] },
+        { name: 'Onix', height: 880, type: ['rock', 'ground'] },
+        { name: 'Ekans', height: 200, type: ['poison'] },
+    ]
 
-pokemonList.forEach(function(pokemon){
-    console.log(pokemon);
-});
+    pokemonList.forEach(function (pokemon) {
+        console.log(pokemon);
+    });
 
-function getAll(){
-    return pokemonList;
-}
+    function getAll() {
+        return pokemonList;
+    }
 
-function add(){
-    pokemonList.push(pokemon)
-}
+    function add() {
+        pokemonList.push(pokemon)
+    }
 
-return{
-    getAll:getAll,
-    add:add
-}
+    function addListItem(pokemon) {
+        let list = document.querySelector(".pokemon-list");
+        let listItem = document.createElement("li");
+        let button = document.createElement("button");
+        button.innerText = pokemon.name;
+        button.classList.add("button-class");
+        listItem.appendChild(button);
+        list.appendChild(listItem);
+        addEvent(button, pokemon)
+    }
+
+    function addEvent(button,pokemon) {
+        button.addEventListener('click', function() {
+          showDetails(pokemon);
+        });
+    }
+
+    function showDetails(pokemon) {
+        console.log(pokemon);
+    }
+
+    return {
+        getAll: getAll,
+        add: add,
+        addListItem: addListItem
+    }
 
 })();
 
-pokemonRepository.getAll().forEach(function(pokemon){
-    if(pokemon.height >= 100){
-        document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - Wow, that's big!")
-    } else if(pokemon.height >= 50){
-        document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - That is avarage!")
-    } else{
-        document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - That's so tiny!")
-    }
+pokemonRepository.getAll().forEach(function (pokemon) {
+
+    pokemonRepository.addListItem(pokemon);
+
+    //     if(pokemon.height >= 100){
+    //         document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - Wow, that's big!")
+    //     } else if(pokemon.height >= 50){
+    //         document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - That is avarage!")
+    //     } else{
+    //         document.write("<p>" + pokemon.name + " (height: " + pokemon.height + "cm" + ")" + " - That's so tiny!")
+    //     }
 });
 
 
